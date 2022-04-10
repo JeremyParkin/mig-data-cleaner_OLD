cb944e5da99a9fbbcd9fb10271ea0f316569847a
Jeremy Parkin <djplana@gmail.com>
2022-04-09 23:21:42 -0400

    21

--- main.py
+++ main.py
@@ -782,14 +782,14 @@ elif page == "6: Authors - Outlets":
             if top_auths_by == 'Mentions':
                 auth_outlet_table = traditional[['Author', 'Mentions', 'Impressions']].groupby(
                     by=['Author']).sum().sort_values(
-                    ['Mentions'], ascending=False).reset_index()
+                    ['Mentions', 'Impressions'], ascending=False).reset_index()
                 auth_outlet_table['Outlet'] = ''
                 auth_outlet_todo = auth_outlet_table
 
             if top_auths_by == 'Impressions':
                 auth_outlet_table = traditional[['Author', 'Mentions', 'Impressions']].groupby(
                     by=['Author']).sum().sort_values(
-                    ['Impressions'], ascending=False).reset_index()
+                    ['Impressions', 'Mentions'], ascending=False).reset_index()
                 auth_outlet_table['Outlet'] = ''
                 auth_outlet_todo = auth_outlet_table
             # st.write("Auth Outlet Table")
